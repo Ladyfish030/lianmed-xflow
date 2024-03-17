7ef68b0620deeb0a50726b88315dbe87648591b4
Ladyfish030 <2227353992@qq.com>
2024-03-17 23:14:58 +0800

    init homevie

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import BaseHeader from '../components/BaseHeader.vue'
+import BaseSide from '../components/BaseSide.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <base-header></base-header>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <base-side></base-side>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
+
+<style scoped></style>
