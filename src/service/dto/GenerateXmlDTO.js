ac84d33ece7710f6a8bdbad1b861c2407683d242
cartoonwu <jiadongwucs@163.com>
2024-07-05 19:15:13 +0800

    修复GenerateXmlDTO文件错误

--- src/service/dto/GenerateXmlDTO.js
+++ src/service/dto/GenerateXmlDTO.js
@@ -10,23 +10,24 @@ import { GlobalConfigTypeInDetail } from '../../enums/GlobalConfigTypeInDetail'
 
 export function formatGenerateXmlData() {
   var result = {
-    globalConfig: [],
-    nodes: [],
+      globalConfig: [],
+      nodes: [],
   }
 
-  const configMappings = {
-    [GlobalConfigTypeInDetail.DATABASE_MYSQL_CONFIG]: ConfigDTO.formatMySQL,
-    [GlobalConfigTypeInDetail.DATABASE_SQLSERVER_CONFIG]:
-      ConfigDTO.formatSQLServer,
-    [GlobalConfigTypeInDetail.DATABASE_ORACLE_CONFIG]: ConfigDTO.formatOracle,
-    [GlobalConfigTypeInDetail.DATABASE_POSTGRESQL_CONFIG]:
-      ConfigDTO.formatPostgreSQL,
-    [GlobalConfigTypeInDetail.LISTENER_CONFIG]: ConfigDTO.formatListener,
-  }
+  // const configMappings = {
+  //   [GlobalConfigTypeInDetail.DATABASE_MYSQL_CONFIG]: ConfigDTO.formatMySQL,
+  //   [GlobalConfigTypeInDetail.DATABASE_SQLSERVER_CONFIG]: ConfigDTO.formatSQLServer,
+  //   [GlobalConfigTypeInDetail.DATABASE_ORACLE_CONFIG]: ConfigDTO.formatOracle,
+  //   [GlobalConfigTypeInDetail.DATABASE_POSTGRESQL_CONFIG]: ConfigDTO.formatPostgreSQL,
+  //   [GlobalConfigTypeInDetail.LISTENER_CONFIG]: ConfigDTO.formatListener,
+  //   [GlobalConfigTypeInDetail.REQUEST_CONFIG]: ConfigDTO.formatRequest,
+  // }
+
   const nodeMappings = {
     [NodeType.FLOW]: NodeDTO.formatFlow,
     [NodeType.DATABASE]: NodeDTO.formatDatabase,
     [NodeType.LISTENER]: NodeDTO.formatListener,
+    [NodeType.REQUEST]: NodeDTO.formatRequest,
     [NodeType.CHOICE]: NodeDTO.formatChoice,
     [NodeType.CHOICEWHEN]: NodeDTO.formatChoiceWhen,
     [NodeType.CHOICEDEFAULT]: NodeDTO.formatChoiceDefault,
@@ -37,36 +38,25 @@ export function formatGenerateXmlData() {
     [NodeType.SETPAYLOAD]: NodeDTO.formatSetPayload,
   }
 
-  const configList = globalConfigList.value
-  for (const config of configList) {
-    var type = config.type
-    if (type == GlobalConfigTypeInGeneral.DATABASE_CONFIG) {
-      type = config.connection
-    }
+  // const configList = globalConfigList.value
+  // for (const config of configList) {
+  //   var type = config.type
+  //   if (type == GlobalConfigTypeInGeneral.DATABASE_CONFIG) {
+  //     type = config.connection
+  //   }
 
-    const configMappings = {
-      [GlobalConfigTypeInDetail.DATABASE_MYSQL_CONFIG]: ConfigDTO.formatMySQL,
-      [GlobalConfigTypeInDetail.DATABASE_SQLSERVER_CONFIG]:
-        ConfigDTO.formatSQLServer,
-      [GlobalConfigTypeInDetail.DATABASE_ORACLE_CONFIG]: ConfigDTO.formatOracle,
-      [GlobalConfigTypeInDetail.DATABASE_POSTGRESQL_CONFIG]:
-        ConfigDTO.formatPostgreSQL,
-      [GlobalConfigTypeInDetail.LISTENER_CONFIG]: ConfigDTO.formatListener,
-      [GlobalConfigTypeInDetail.REQUEST_CONFIG]: ConfigDTO.formatRequest,
-    }
-    const nodeMappings = {
-      [NodeType.FLOW]: NodeDTO.formatFlow,
-      [NodeType.DATABASE]: NodeDTO.formatDatabase,
-      [NodeType.LISTENER]: NodeDTO.formatListener,
-      [NodeType.CHOICE]: NodeDTO.formatChoice,
-      [NodeType.CHOICEWHEN]: NodeDTO.formatChoiceWhen,
-      [NodeType.CHOICEDEFAULT]: NodeDTO.formatChoiceDefault,
-      [NodeType.FOREACH]: NodeDTO.formatForEach,
-      [NodeType.SUBFLOW]: NodeDTO.formatSubFlow,
-      [NodeType.LOGGER]: NodeDTO.formatLogger,
-      [NodeType.FLOWREFERENCE]: NodeDTO.formatFlowReference,
-      [NodeType.SETPAYLOAD]: NodeDTO.formatSetPayload,
-      [NodeType.REQUEST]: NodeDTO.formatRequest,
+  //   if (configMappings.hasOwnProperty(type)) {
+  //     const targetConfig = configMappings[type](config)
+  //     result.globalConfig.push(targetConfig)
+  //   }
+  // }
+
+  const nodeList = nodes.value
+  for (const node of nodeList) {
+    const type = node.type
+    if (nodeMappings.hasOwnProperty(type)) {
+      const targetNode = nodeMappings[type](node)
+      result.nodes.push(targetNode)
     }
   }
 
